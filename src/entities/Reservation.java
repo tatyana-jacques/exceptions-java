a4e5f6572ed466d60741fc9e70f33f502ed5b4db
Tatyana Jacques <taty.aj@gmail.com>
2025-07-07 11:37:53 -0300

    Bad solution.

--- src/entities/Reservation.java
+++ src/entities/Reservation.java
@@ -46,9 +46,20 @@ public class Reservation {
 		
 	}
 	
-	public void updateDates( LocalDate checkIn, LocalDate checkOut) {
-		this.checkIn = checkIn;
-		this.checkOut = checkOut;
+	public String updateDates( LocalDate checkIn, LocalDate checkOut) {
+		
+		LocalDate now = LocalDate.now();
+		if(checkIn.isBefore(now) || checkOut.isBefore(now)) {
+			return "Reservation dates for update must be future dates";
+		}
+		if(!checkOut.isAfter(checkIn)) {
+			return "Check-out date must be after check-in date";
+			
+		}
+	
+			this.checkIn = checkIn;
+			this.checkOut = checkOut;
+			return null;		
 	}
 
 	@Override
