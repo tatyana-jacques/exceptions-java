c4d90da71a7f950efd60f69da47d64a54eae71b0
Tatyana Jacques <taty.aj@gmail.com>
2025-07-07 10:52:37 -0300

    Change reservation.

--- src/entities/Reservation.java
+++ src/entities/Reservation.java
@@ -1,10 +1,70 @@
 package entities;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Reservation {
 	
-	public Integer roomNumber;
-	public LocalDateTime chekin;
+	private Integer roomNumber;
+	private LocalDate checkIn;
+	private LocalDate checkOut;
+	
+	private static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
+	public Reservation() {
+		
+	}
+
+	public Reservation(Integer roomNumber, LocalDate chekIn, LocalDate checkOut) {
+		super();
+		this.roomNumber = roomNumber;
+		this.checkIn = chekIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+
+	public void setChekIn(LocalDate chekIn) {
+		this.checkIn = chekIn;
+	}
+
+	public LocalDate getCheckOut() {
+		return checkOut;
+	}
+	
+	public long duration() {
+		
+		return ChronoUnit.DAYS.between(checkIn, checkOut);
+		
+	}
+	
+	public void updateDates( LocalDate checkIn, LocalDate checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	@Override
+	public String toString() {
+		return "Room "
+				+ roomNumber
+				+ ", checkin: "
+				+ fmt.format(checkIn)
+				+ ", checkout: "
+				+ fmt.format(checkOut)
+				+ ", "
+				+ duration()
+				+ "nights.";
+		
+	}
+	
+	
 
 }
