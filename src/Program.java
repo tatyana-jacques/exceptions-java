9fbb53e98bcb4add61248cc4a2519c7e7f4b1deb
Tatyana Jacques <taty.aj@gmail.com>
2025-07-07 10:03:17 -0300

    First commit.

---
+++ src/Program.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
+		
+		System.out.print("Room number: ");
+		int roomNumber = sc.nextInt();
+		
+
+	}
+
+}
---
+++ src/entities/Reservation.java
@@ -0,0 +1,10 @@
+package entities;
+
+import java.time.LocalDateTime;
+
+public class Reservation {
+	
+	public Integer roomNumber;
+	public LocalDateTime chekin;
+
+}
